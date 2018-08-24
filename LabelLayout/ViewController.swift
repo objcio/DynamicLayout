0fdbbfc16163c8bcdc65462baebf1c9d02fedaa3
Florian Kugler <mail@floriankugler.com>
2018-08-24 15:30:16 +0200

    Episode 2

--- LabelLayout/ViewController.swift
+++ LabelLayout/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 indirect enum Layout {
     case view(UIView, Layout)
-    case newline(Layout)
+    case space(CGFloat, Layout)
+    case newline(space: CGFloat, Layout)
     case choice(Layout, Layout)
     case empty
 }
@@ -30,9 +31,12 @@ extension Layout {
                 lineHeight = max(lineHeight, size.height)
                 result.insert(view)
                 el = next
-            case let .newline(next):
+            case let .space(space, next):
+                p.x += space
+                el = next
+            case let .newline(space, next):
                 p.x = 0
-                p.y += lineHeight
+                p.y += lineHeight + space
                 lineHeight = 0
                 el = next
             case let .choice(first, second):
@@ -59,7 +63,10 @@ extension Layout {
                     return false
                 }
                 el = next
-            case let .newline(next):
+            case let .space(space, next):
+                x += space
+                el = next
+            case let .newline(_, next):
                 x = 0
                 el = next
             case let .choice(first, second):
@@ -118,6 +125,49 @@ func label(text: String, size: UIFontTextStyle, multiline: Bool = false) -> UILa
     return label
 }
 
+extension Array where Element == Layout {
+    func horizontal(space: CGFloat = 0) -> Layout {
+        guard var result = last else { return .empty }
+        for l in dropLast().reversed() {
+            if space > 0 {
+                result = .space(space, result)
+            }
+            result = l + result
+        }
+        return result
+    }
+    
+    func vertical(space: CGFloat = 0) -> Layout {
+        guard var result = last else { return .empty }
+        for l in dropLast().reversed() {
+            result = l + .newline(space: space, result)
+        }
+        return result
+    }
+}
+
+extension UIView {
+    var layout: Layout {
+        return .view(self, .empty)
+    }
+}
+
+func +(lhs: Layout, rhs: Layout) -> Layout {
+    switch lhs {
+    case let .view(v, next): return .view(v, next + rhs)
+    case let .space(space, next): return .space(space, next + rhs)
+    case let .newline(space, next): return .newline(space: space, next + rhs)
+    case let .choice(l, r): return .choice(l + rhs, r + rhs)
+    case .empty: return rhs
+    }
+}
+
+extension Layout {
+    func or(_ other: Layout) -> Layout {
+        return .choice(self, other)
+    }
+}
+
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -126,13 +176,12 @@ class ViewController: UIViewController {
         let episodeNumber = label(text: "Episode 123", size: .caption1)
         let episodeDate = label(text: "September 23", size: .caption1)
 
+        let metadata = [episodeNumber.layout, episodeDate.layout]
         let layout: Layout =
             .view(titleLabel,
-            .newline(
-            .choice(
-                .view(episodeNumber, .view(episodeDate, .empty)),
-                .view(episodeNumber, .newline(.view(episodeDate, .empty)))
-            )))
+            .newline(space: 10,
+            metadata.horizontal(space: 20).or(metadata.vertical())
+            ))
 
         let container = LayoutView(layout)
         container.translatesAutoresizingMaskIntoConstraints = false
