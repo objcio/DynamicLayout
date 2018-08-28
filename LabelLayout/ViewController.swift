c6e788d4fb7a81bec8ab771ce8fc86c6204c8cad
Chris Eidhof <chris@eidhof.nl>
2018-08-28 13:50:28 +0200

    Checkpoint

--- LabelLayout/ViewController.swift
+++ LabelLayout/ViewController.swift
@@ -34,6 +34,92 @@ extension UILabel {
     }
 }
 
+indirect enum Layout {
+    case view(UIView, Layout)
+    case newline(Layout)
+    case choice(Layout, Layout)
+    case empty
+}
+
+extension Layout {
+    func apply(containerWidth: CGFloat) -> [UIView] {
+        var result: [UIView] = []
+        var origin: CGPoint = .zero
+        var current: Layout = self
+        var lineHeight: CGFloat = 0
+        while true {
+        switch current {
+            case let .view(v, rest):
+                result.append(v)
+                let availableWidth = containerWidth - origin.x
+                let size = v.sizeThatFits(CGSize(width: availableWidth, height: .greatestFiniteMagnitude))
+                v.frame = CGRect(origin: origin, size: size)
+                lineHeight = max(lineHeight, size.height)
+                origin.x += size.width
+                current = rest
+            case let .newline(rest):
+                origin.x = 0
+                origin.y += lineHeight
+                lineHeight = 0
+                current = rest
+            case let .choice(first, second):
+                if first.fits(currentX: origin.x, containerWidth: containerWidth) {
+                    current = first
+                } else {
+                    current = second
+                }
+            case .empty:
+                return result
+            }
+        }
+    }
+    
+    func fits(currentX: CGFloat, containerWidth: CGFloat) -> Bool {
+        var x = currentX
+        var current: Layout = self
+        while true {
+            switch current {
+            case let .view(v, rest):
+                let availableWidth = containerWidth - x
+                let size = v.sizeThatFits(CGSize(width: availableWidth, height: .greatestFiniteMagnitude))
+                x += size.width
+                if x >= containerWidth { return false }
+                current = rest
+            case let .newline(rest):
+                x = 0
+                current = rest
+            case let .choice(first, second):
+                if first.fits(currentX: x, containerWidth: containerWidth) {
+                    return true
+                } else {
+                    current = second
+                }
+            case .empty:
+                return true
+            }
+        }
+    }
+}
+
+final class LayoutContainer: UIView {
+    let layout: Layout
+    init(_ layout: Layout) {
+        self.layout = layout
+        super.init(frame: .zero)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(setNeedsLayout), name: Notification.Name.UIContentSizeCategoryDidChange, object: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        let views = layout.apply(containerWidth: bounds.width)
+        setSubviews(views)
+    }
+}
+
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,17 +127,21 @@ class ViewController: UIViewController {
         let titleLabel = UILabel(text: "Building a Layout Library", size: .headline, multiline: true)
         let episodeNumber = UILabel(text: "Episode 123", size: .body)
         let episodeDate = UILabel(text: "September 23", size: .body)
-
-        let horizontalStack = UIStackView(arrangedSubviews: [episodeNumber, episodeDate])
-        let verticalStack = UIStackView(arrangedSubviews: [titleLabel, horizontalStack])
-        verticalStack.axis = .vertical
         
-        view.addSubview(verticalStack)
-        verticalStack.translatesAutoresizingMaskIntoConstraints = false
+        let horizontal = Layout.view(episodeNumber, Layout.view(episodeDate, .empty))
+        let vertical = Layout.view(episodeNumber, .newline(Layout.view(episodeDate, .empty)))
+        let layout = Layout.view(titleLabel, .newline(
+            .choice(horizontal, vertical)
+            ))
+        
+        let container = LayoutContainer(layout)
+        container.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(container)
         view.addConstraints([
-            verticalStack.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
-            verticalStack.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            verticalStack.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            container.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            container.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            container.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
         ])
        
     }
